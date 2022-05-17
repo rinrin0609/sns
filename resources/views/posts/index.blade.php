1dad216dc7db62330ecedf20201db225692f7a33
LAPTOP-NJDFI52M\paper <tambmeg.0609@gmail.com>
2022-05-17 21:52:58 +0900

    index.blade.php

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,8 +2,16 @@
 
 @section('content')
 {{ Form::open(['url' => '/post/create'])}}
- {{ Form::text('posts',null, ['class' => 'form-control', 'placeholder' => '何をつぶやこうか'])}}
-{{ Form::input()}}
+{{ Form::text('posts',null, ['class' => 'form-control', 'placeholder' => '何をつぶやこうか'])}}
+{{ Form::submit('投稿する')}}
 {{ Form::close()}}
+
+foreach($post_create as $posts)
+<tr>
+  <td>{{ $user_id ->'user_id'}}</td>
+  <td>{{ $post ->'post'}}</td>
+  <td>{{ $created_at ->'created_at'}}</td>
+  <td><img src = "/images/dawn.png{{ $image -> image }}"></td>
+</tr>
 
 @endsection
