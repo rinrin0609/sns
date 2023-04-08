56a78f7d9c0c0a370678bfd85b5e9654489c046d
LAPTOP-NJDFI52M\paper <tambmeg.0609@gmail.com>
2023-04-08 21:57:57 +0900

    2023/4/8更新

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -22,22 +22,11 @@
     <div class="edit-content">
     <a href="" class="modalopen" data-target="{{ $list->id }}">
     {{ csrf_field() }}
-      <img class="edit-img" src="/images/edit.png" alt="更新">
+    <img class="edit-img" src="/images/edit.png" alt="更新">
     </a>
-    </div>
-    </div>
-    <div class="post-icons">
-    <div class="modal-main js-modal" id="{{ $list->id }}">
-            {{ Form::open(['url' => '/post/update']) }}
-            {{ Form::hidden('id', $list->id) }}
-            {{ Form::input('text', 'upPost', $list->post, ['required', 'class' => 'form-control']) }}
-            <input type="image" src="/images/edit.png" alt="更新ボタン">
-            {{ Form::close() }}
-    {{ csrf_field() }}
-    </div>
-    </div>
     <a href="/post/{{$list ->id}}/delete"onclick="return confirm('こちらの投稿を削除してもよろしいでしょうか？')"> <img class="edit-img" src="{{ asset('images/trash_h.png') }}" alt="削除" ></a>
-  @endif
-  @endforeach
+  </div>
 </div>
+@endif
+@endforeach
 @endsection
